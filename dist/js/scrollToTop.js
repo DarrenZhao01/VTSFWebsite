929f1c4c6e3073756d4ec1d95c7994ccc0dd29cd
Calvin <calvinlu@live.com>
2022-12-30 17:17:34 -0800

    Add mobile support for index.html

---
+++ dist/js/scrollToTop.js
@@ -0,0 +1,44 @@
+const timeToTop = 500; // ms
+const scrollInterval = 3; // ms
+
+function easeInOutQuart(x) {
+    return x < 0.5 ? 8 * x * x * x * x : 1 - Math.pow(-2 * x + 2, 4) / 2;
+}
+
+function initScrollToTop() {
+    const currentScrollYPosition = window.scrollY;
+    let totalScrolls = Math.ceil(timeToTop / scrollInterval);
+    let scrollPositions = [];
+
+    for (let i = 1; i <= totalScrolls; i++) {
+        scrollPositions.push(currentScrollYPosition - (easeInOutQuart(i / totalScrolls) * currentScrollYPosition));
+    }
+
+    scrollToTop(scrollPositions.values(), scrollInterval);
+}
+
+function scrollToTop(iterator, msDelay) {
+    let result = iterator.next();
+    
+    window.scrollTo(0, result.value);
+
+    if (!result.done) {
+        setTimeout(() => {
+            scrollToTop(iterator, msDelay);
+        }, msDelay);
+    }
+}
+
+addEventListener("scroll", (event) => {
+    const elements = document.getElementsByClassName("scroll-to-top");
+
+    if (window.scrollY < window.innerHeight) {
+        for (let i = 0; i < elements.length; i++) {
+            elements[i].classList.add("opacity-0");
+        }
+    } else {
+        for (let i = 0; i < elements.length; i++) {
+            elements[i].classList.remove("opacity-0");
+        }
+    }
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,12 +9,14 @@ module.exports = {
         "peach1": "#ffd8b7",
         "peach2": "rgb(212, 110, 16)",
         "peach3": "orange",
-        "red1": "#980A00"
+        "red1": "#980A00",
+        "green1": "#60A42C"
       },
       spacing: {
         "30rem": "30rem",
         "40rem": "40rem",
-        "1500px": "1500px"
+        "1500px": "1500px",
+        "full": "100%"
       },
       listStyleType: {
         "circle": "circle",
