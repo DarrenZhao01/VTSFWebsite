0544bbf70062b984466786b02bae4a3212a2020d
Calvin <calvinlu@live.com>
2022-12-31 22:09:53 -0800

    Finish navbar

--- dist/js/menuDropdown.js
+++ dist/js/menuDropdown.js
@@ -1,7 +1,19 @@
 function openMenuDropdown() {
+    document.body.classList.add("overflow-hidden");
 
+    let menuDropdowns = document.getElementsByClassName("menu-dropdown");
+
+    for (let i = 0; i < menuDropdowns.length; i++) {
+        menuDropdowns[i].classList.remove("hide");
+    }
 }
 
 function closeMenuDropdown() {
-    
+    document.body.classList.remove("overflow-hidden");
+
+    let menuDropdowns = document.getElementsByClassName("menu-dropdown");
+
+    for (let i = 0; i < menuDropdowns.length; i++) {
+        menuDropdowns[i].classList.add("hide");
+    }
 }
