9842dcdea79b93a72f81817cf953d1cab835762e
DarrenZhao01 <zhaod579@gmail.com>
2023-03-15 14:49:17 -0700

    Moved Pop-up from Tutors to Index

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 export * from "./util/menuDropdown.js";
 export * from "./util/scrollToTop.js";
+export * from "./util/popup";
 
 import "./util/carousel.js";
 
--- src/js/tutors.js
+++ src/js/tutors.js
@@ -1,6 +1,6 @@
 export * from "./util/menuDropdown.js";
 export * from "./util/scrollToTop.js";
-export * from "./util/popup";
+
 
 import "../css/style.css";
 import "../css/tutorsStyle.css";
